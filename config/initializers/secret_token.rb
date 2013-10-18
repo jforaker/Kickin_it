f81f376cd672f7fef2888ad933389b64133603c5
jforaker <jake@chalkable.com>
2013-10-18 16:17:16 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KickinIt::Application.config.secret_key_base = 'f1cc0c8e40b177c5731647827d20220a45edad63ea84130cd1df6b7722a21ae852fabe3e87b50133dbf717790f43da2b57d95e4cde7a063ae2206dcbbeb487c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KickinIt::Application.config.session_store :cookie_store, key: '_Kickin_it_session'
