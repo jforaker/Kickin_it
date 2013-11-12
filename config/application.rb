d1a7bb3d9c454c0b94cf8573bd72f70d8c8d3cf8
jforaker <jake@chalkable.com>
2013-11-12 13:28:30 -0500

    compiling assets with:   RAILS_ENV=production bundle exec rake assets:precompile

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module KickinIt
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
+    config.assets.precompile += [
+        'application.css'
+    ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
